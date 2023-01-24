d215b14de2d6c4fa263f85177f6ea35786970e90
crxsx0 <crxsx.0@gmail.com>
2023-01-23 23:48:42 -0300

    index.js lee el archivo xlsx y ahora el bot contesta al comando info con la data de la primera zapatilla

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, DiscordAPIError } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const XLSX = require('xlsx');
+
+const nombreTilla = []
+const precioTilla = []
+
+//funcion para leer archivo excel
+function leerExcel(ruta){
+  const workbook = XLSX.readFile(ruta);
+  const workbookSheets = workbook.SheetNames;
+  console.log(workbookSheets);
+  const sheet = workbookSheets[0];
+  const dataEXcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+  //console.log(dataEXcel)
+  for (const itemFila of dataEXcel ){
+    nombreTilla.push(itemFila['Nombre']);
+    precioTilla.push(itemFila['Precio']);
+  }
+}
+
+leerExcel('nike.xlsx')
+
+//npx nodemon index.js
 
 client.on('ready', () => {
   console.log(`el servidor ${client.user.tag} esta listo!!!`);
@@ -33,34 +54,32 @@ client.on('interactionCreate', (inn) => {
   if (inn.isCommand() && inn.commandName === 'hola') {
     inn.reply('chao')
   } 
+
+  if (inn.isCommand() && inn.commandName === 'info') {
+    /*const alert = new Discord.MessageEmbed
+    .setTitle(nombreTilla[0])//titulo del msg
+    .setAuthor(message.member.displayName, message.author.displayAvatarUrl())
+    .setColor(0xE964ED)//color laterales
+    //.setThumbnail()//es para poner un gif 
+    .setDescription(precioTilla[0])//desdripcion de este contenido
+    //podemos agregar mas contenido pero ahi vamos viendo*/
+    inn.reply(nombreTilla[0] + ', ' + precioTilla[0])
+  } 
 });
 
 //mensaje embed
 client.on("message", messeger =>{
-  if(message.content === ("info")){
+  if(message.content === ("-info")){
     const alert = new MessageEmbed(message.content)
-    .setTitle()//titulo del msg
+    .setTitle(nombreTilla[0])//titulo del msg
     .setAuthor(message.member.displayName, message.author.displayAvatarUrl())
     .setColor(0xE964ED)//color laterales
-    .setThumbnail()//es para poner un gif 
-    .setDescription("")//desdripcion de este contenido
+    //.setThumbnail()//es para poner un gif 
+    .setDescription(precioTilla[0])//desdripcion de este contenido
     //podemos agregar mas contenido pero ahi vamos viendo 
     message.channel.send(alert);
   }
 })
-
-//funcion para leer archivo excel
-function leerExcel(ruta){
-  const workbook = XLSX.readFile(ruta);
-  const workbookSheets = workbook.SheetNames;
-  //console.log(workbookSheets);
-  const sheet = workbookSheets[0];
-  const dataEXcel = XLSX.utils.sheet_to_json(workbook.sheets[sheet]);
-  //console.log(dataEXcel)
-  for (const itemFila of dataEXcel ){
-    console.log(itemFila[])
-  }
-}
-
-leerExcel('nike.xlsx')
-client.login();
+console.log(nombreTilla[0]);
+console.log(precioTilla[0]);
+client.login('MTA2NjQ0MzI5NzQ5MDQ3NzEzNg.Gl8F1Q.GBt6pv3RFZRSdgI6Fp4txYTIvVFm2XZ070TPMI');
