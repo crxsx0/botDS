d870170b06b459676ad3e7ac70481a1f09a7763e
Xrd3000 <xrdd3000@gmail.com>
2023-01-25 18:47:33 -0300

    mensajes embed funcionando

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ require('./server.js');
 
 const nombreTilla = []
 const precioTilla = []
+const fechaTilla = []
+const fotoTilla = []
+const snkrs = []
+const linkTilla = []
 
 function leerExcel(ruta){
     const workbook = XLSX.readFile(ruta);
@@ -28,12 +32,20 @@ function leerExcel(ruta){
     for (const itemFila of dataEXcel ){
       nombreTilla.push(itemFila['Nombre']);
       precioTilla.push(itemFila['Precio']);
+      fechaTilla.push(itemFila['Fecha']);
+      fotoTilla.push(itemFila['Foto']);
     }
     console.log('Archivo leido exitosamente');
 };
 
+function listaSNKRS (){
+    for (i = 0; i < nombreTilla.length; i ++){
+        snkrs.push(i +1 + '. ' + nombreTilla[i]);
+    }
+};
+
 async function encenderBot () {
-    await init();
+    //await init();
     setTimeout(() => {
         console.log('** Listo **');
         leerExcel('nike.xlsx')
@@ -45,8 +57,17 @@ encenderBot()
 
 client.on('ready', () => {
     console.log('El bot esta disponible');
+    listaSNKRS();
+    const listt = String(snkrs.toString())
+    console.log(snkrs);
     client.on('messageCreate', async (message) =>{
         if(message.author.bot) return
-        message.channel.send(nombreTilla[0] + ' ' + precioTilla[0]);
+
+        const embed = new EmbedBuilder()
+            .setTitle('SNKRS')
+            .setDescription(nombreTilla[0] + '\n' + precioTilla[0] + '\n' + fechaTilla[0])
+            .setThumbnail(fotoTilla[0])
+        
+            message.channel.send({ embeds: [embed] })
     });
 })
